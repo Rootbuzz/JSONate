1ac05ecac70c4ab349176a406925853473913dee
James Robert <jim.mixtake@gmail.com>
2011-08-29 14:18:29 -0400

    should work a little better

--- jsonate/http.py
+++ jsonate/http.py
@@ -6,5 +6,5 @@ class JsonateResponse(HttpResponse):
     Subclass of HttpResponse that turns just about anything into JSON
     via jsonate() and returns the result with mimetype "application/json"
     """
-    def __init__(self, data):
-        super(JsonateResponse, self).__init__(content=jsonate(data), mimetype='application/json')
+    def __init__(self, content, mimetype='application/json', *args, **kwargs):
+        super(JsonateResponse, self).__init__(jsonate(content), mimetype, *args, **kwargs)
