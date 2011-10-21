ef8dac5e30dafa4331b79b65165888c5a3c2826f
James Robert <jim.mixtake@gmail.com>
2011-10-21 11:43:44 -0400

    fix issue #2

--- jsonate/utils.py
+++ jsonate/utils.py
@@ -1,4 +1,8 @@
-import json
+try:
+    import json
+except ImportError:
+    from simplejson import json
+     
 from jsonate.json_encoder import JsonateEncoder
 
 def jsonate(obj):
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jsonate',
-    version='0.1.5',
+    version='0.1.6',
     
     author='James Robert',
     author_email='jiaaro@gmail.com',
